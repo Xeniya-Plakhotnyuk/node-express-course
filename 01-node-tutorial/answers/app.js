ad5e8ef4b98fc552f1d455d18053bc330dd0071c
XeniyaZ <kseniya10081989@gmail.com>
2024-02-18 13:02:49 -0600

    feat: answers for lesson 1

--- 01-node-tutorial/answers/app.js
+++ 01-node-tutorial/answers/app.js
@@ -1,11 +1,1 @@
-const amount = 12
-
-if (amount<10){
-console.log('small number')
-}
-else{
-    console.log('large number')
-}
-
-console.log(`hey, it's my first Node app!`)
-
+console.log("Node tutorials 2024!")
