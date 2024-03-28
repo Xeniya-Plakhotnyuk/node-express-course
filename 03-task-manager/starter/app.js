0bf4a09132b44d539d24bb1206e2d30ea7282402
XeniyaZ <kseniya10081989@gmail.com>
2024-03-28 13:30:20 -0500

    week 4 assignments

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,37 @@
-console.log('Task Manager App')
+const express = require ('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+mongoose.set('strictQuery', false)
+
+app.get('/hello', (req, res) =>{
+    res.send("My Task Manager App")
+})
+
+
+app.use('/api/v1/tasks', tasks)
+
+const PORT = 7000 || process.env.port
+
+app.use(express.json())
+app.use(cors())
+
+mongoose
+.connect(process.env.MONGODB_LINK, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+.then(() => console.log(`We are CONNECTED!`))
+.catch((err) => console.log(err))
+
+// app.use(router)
+
+
+app.listen(PORT, ()=>{
+    console.log(`Sever listening on port ${PORT}`)
+})
