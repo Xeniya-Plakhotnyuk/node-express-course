93c8b97c31946d4ee611d4421056aea8bf0eef14
XeniyaZ <kseniya10081989@gmail.com>
2024-04-03 12:51:39 -0500

    week 6 assignments completed

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,33 +3,50 @@ const app = express()
 const tasks = require('./routes/tasks')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 require('dotenv').config()
 mongoose.set('strictQuery', false)
 
-app.get('/hello', (req, res) =>{
-    res.send("My Task Manager App")
-})
-
+// routes
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+// server 
 
 const PORT = 7000 || process.env.port
 
-app.use(express.json())
-app.use(cors())
 
-mongoose
-.connect(process.env.MONGODB_LINK, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-.then(() => console.log(`We are CONNECTED!`))
-.catch((err) => console.log(err))
+const start = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_LINK, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      });
+      console.log(`Connected to MongoDB!`);
+      
+      app.listen(PORT, () =>
+        console.log(`Server is listening on port ${PORT}...`)
+      );
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+  
+  start()
 
 
-app.listen(PORT, ()=>{
-    console.log(`Sever listening on port ${PORT}`)
-})
+
+//   for Postman:   http://localhost:7000/api/v1/tasks
+
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -2,8 +2,21 @@ const express = require('express')
 
 const router  = express.Router()
 
-router.route('/').get((req, res) =>{
-    res.send('Get all items')
-})
+const {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+    editTask,
+  } = require('../controllers/tasks')
+  
+  router.route('/')
+  .get(getAllTasks)
+  .post(createTask)
+
+  router.route('/:id')
+  .get(getTask)
+  .patch(updateTask).delete(deleteTask)
 
 module.exports = router
