979b717cc8837df2c5dffccf658717e1961b9b2a
XeniyaZ <kseniya10081989@gmail.com>
2024-04-11 13:08:49 -0500

    week7 assesment done

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,46 @@
-console.log('04 Store API')
+require('dotenv').config()
+require('express-async-errors')
+
+const cors = require('cors')
+
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const express = require('express')
+const app = express()
+
+// middlewares
+
+app.use(express.json())
+app.use(cors())
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+//routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+  });
+
+app.use('/api/v1/products', productsRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+
+const port = process.env.PORT || 3000
+
+
+
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI);
+      app.listen(port, () => console.log(`My server is listening port ${port}...`))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  start()
--- 04-store-api/starter/db/connect.js
+++ 04-store-api/starter/db/connect.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
+
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
+  }).then(() => {
+    console.log('We are Connected to MongoDB');
+  }).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 }
 
 module.exports = connectDB
